bb2703d784bf5dd2b200962d13169d727b031456
Titiushko <titiushko@gmail.com>
2019-06-18 01:34:45 -0600

    :monkey: Opción para realizar reintentos sin salir del programa

--- bin2byte_ex.asm
+++ bin2byte_ex.asm
@@ -14,10 +14,12 @@ includelib \masm32\lib\debug.lib
 includelib \masm32\lib\user32.lib
 
 .data
-buffer 			db 127 dup(" "),0
+buffer1			db 127 dup(" "),0
+buffer2			db 2 dup(" "),0
 txt_title 		db "-----------EJEMPLO DE Bin2Byte_Ex-----------",0
 txt_description db "Es una conversion de alta velocidad de un valor de notacion BINARIA a un valor de BYTE",0
-message 		db "Digitar un valor en BINARIO: ",0
+message1 		db "Digitar un valor en BINARIO (ejemplo: 00000010): ",0
+message2 		db "Hacer otra prueba? (S/N): ",0
 result_p1 		db "Resultado: ",0
 result_p2 		db " se pudo realizar la conversion a BYTE",0
 result_si 		db "si",0
@@ -32,11 +34,11 @@ start:
 	invoke locate,0,1
 	invoke StdOut,ADDR txt_description
 	invoke locate,0,4
-	invoke StdOut,ADDR message
-	invoke StdIn,ADDR buffer,LENGTHOF buffer
+	invoke StdOut,ADDR message1
+	invoke StdIn,ADDR buffer1,LENGTHOF buffer1
 	invoke locate,0,5
 	invoke StdOut,ADDR result_p1
-	invoke bin2byte_ex,ADDR buffer
+	invoke bin2byte_ex,ADDR buffer1
 	;PrintDec EAX, "Valor en Decimal" ; Si EAX = -1, entonces no se pudo realizar la conversión
 	CMP EAX,EBX
 	JE INCORRECTO
@@ -48,6 +50,16 @@ INCORRECTO:
 	JMP FIN
 FIN:
 	invoke StdOut,ADDR result_p2
+	;SALIR?
+	invoke locate,0,7
+	invoke StdOut,ADDR message2
+	invoke StdIn,ADDR buffer2,LENGTHOF buffer2
+	CMP buffer2,"N"
+	JE finish
+	CMP buffer2,"n"
+	JE finish
+	JNE start
+finish:
 	invoke locate,0,10
 	invoke ExitProcess,0
 	end start
--- hexflip32.asm
+++ hexflip32.asm
@@ -10,10 +10,12 @@ includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
 
 .data
-buffer 			db 127 dup(" "),0
+buffer1			db 127 dup(" "),0
+buffer2			db 2 dup(" "),0
 txt_title 		db "-----------EJEMPLO DE HexFlip32-----------",0
 txt_description db "Convertir un valor HEX normal a un HEX de orden de memoria. Se acepta notacion hexadecimal de formato de ensamblador con la h al final y opcionalmente un 0 inicial. La longitud total de la cadena no debe exceder los 10 bytes, incluyendo h y 0.",0
-message 		db "Digitar un valor en HEX (ejemplo: 01199AAFFh): ",0
+message1 		db "Digitar un valor en HEX (ejemplo: 01199AAFFh): ",0
+message2 		db "Hacer otra prueba? (S/N): ",0
 result 			db "Resultado en HEX de orden de memoria: ",0
 
 .code
@@ -24,12 +26,22 @@ start:
 	invoke locate,0,1
 	invoke StdOut,ADDR txt_description
 	invoke locate,0,6
-	invoke StdOut,ADDR message
-	invoke StdIn,ADDR buffer,LENGTHOF buffer
-	invoke hexflip32,ADDR buffer
+	invoke StdOut,ADDR message1
+	invoke StdIn,ADDR buffer1,LENGTHOF buffer1
+	invoke hexflip32,ADDR buffer1
 	invoke locate,0,7
 	invoke StdOut,ADDR result
-	invoke StdOut,ADDR buffer
-	invoke locate,0,10
+	invoke StdOut,ADDR buffer1
+	;SALIR?
+	invoke locate,0,9
+	invoke StdOut,ADDR message2
+	invoke StdIn,ADDR buffer2,LENGTHOF buffer2
+	CMP buffer2,"N"
+	JE finish
+	CMP buffer2,"n"
+	JE finish
+	JNE start
+finish:
+	invoke locate,0,12
 	invoke ExitProcess,0
 	end start
